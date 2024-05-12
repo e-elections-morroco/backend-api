16fd814a53742c4b1a19840000227730bee89545
ouaillaamiri <laamiri.ouail@etu.uae.ac.ma>
2024-05-12 13:44:43 +0100

    Setup computer vision functionnalite from e-elections-morroco/face-recognition-system Repo

---
+++ computer_vision/compare_image.py
@@ -0,0 +1,107 @@
+import os
+import sys
+
+# Add parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), '.'))
+sys.path.append(parent_dir)
+
+import face_recognition
+from pathlib import Path
+
+from computer_vision.encoding_image import get_image_encoding,get_image_encoding_from_csv
+
+
+def compare_faces(known_image_path: Path, unknown_image_path: Path) -> bool | None:
+    """
+    Compare faces in two images.
+
+    Args:
+        known_image_path (Path): Path to the image file containing the known face.
+        unknown_image_path (Path): Path to the image file containing the unknown face.
+
+    Returns:
+        bool | None: True if faces match, False if faces don't match, None if there's an error.
+
+    Raises:
+        None
+
+    Examples:
+        >>> known_image_path = Path("ouail.jpg")
+        >>> unknown_image_path = Path("messi.jpeg")
+        >>> result = compare_faces(known_image_path, unknown_image_path)
+        >>> if result is not None:
+        >>>     print("Face match:", result)
+    """
+    if not known_image_path.exists() or not unknown_image_path.exists():
+        print("Error: One or both image files do not exist.")
+        return None
+    try:
+
+        # Proceed with face encoding if images loaded successfully
+        known_encoding = get_image_encoding(known_image_path)
+        unknown_encoding = get_image_encoding(unknown_image_path)
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        return True if str(results[0])== "True" else False
+    except IndexError:
+        print("No faces detected in one of the images.")
+        return None
+
+
+def compare_face_use_csv_encoding(known_image_name: str, unknown_image_path: Path, csv_filename: Path) -> bool | None:
+    """
+    Compare faces in an image with known faces in a CSV file.
+
+    Args:
+        known_image_name (str): Name of the known image.
+        unknown_image_path (Path): Path to the image file containing the unknown face.
+        csv_filename (Path): Path to the CSV file containing known image encodings.
+
+    Returns:
+        bool | None: True if faces match, False if faces don't match, None if there's an error.
+
+    Raises:
+        None
+
+    Examples:
+        >>> known_image_name = "ouail.jpg"
+        >>> unknown_image_path = Path("messi.jpeg")
+        >>> csv_filename = Path("test/test_encodings.csv")
+        >>> result = compare_face_use_csv_encoding(known_image_name, unknown_image_path, csv_filename)
+        >>> if result is not None:
+        >>>     print("Face match:", result)
+    """
+    if not unknown_image_path.exists() or not csv_filename.exists():
+        print("Error: Image file or CSV file does not exist.")
+        return None
+    try:
+        known_encoding = get_image_encoding_from_csv(known_image_name, csv_filename)
+        unknown_encoding = get_image_encoding(unknown_image_path)
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+        return True if str(results[0])== "True" else False
+    except IndexError:
+        print("No faces detected in one of the images.")
+        return None
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    import time
+    start = time.time()
+    print( compare_face_use_csv_encoding("ouail.jpg",Path("images/unknown.jpg"),Path("encodings.csv")))
+    end = time.time()
+    print("Time:",end-start)
+
+    start = time.time()
+    print(compare_faces(Path("images/ouail.jpg"),Path("images/unknown.jpg")))
+    end = time.time()
+    print("Time:",end-start)
+    
+
+
+
+
---
+++ computer_vision/encoding_image.py
@@ -0,0 +1,146 @@
+import sys
+import os
+# Add parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), '.'))
+sys.path.append(parent_dir)
+
+import csv
+import face_recognition
+import cv2
+from pathlib import Path
+
+import numpy as np
+
+def get_image_encoding(image_path: Path) -> list | None:
+    """
+    Get face encoding from an image.
+
+    Args:
+        image_path (Path): Path to the image file.
+
+    Returns:
+        np.ndarray | None: The encoding of the specified image if found, None otherwise.
+
+    Raises:
+        None
+
+    Examples:
+        >>> image_path = Path("ouail.jpg")
+        >>> encoding = get_image_encoding(image_path)
+        >>> if encoding is not None:
+        >>>     print("Face encoding:", encoding)
+    """
+    if not image_path.exists():
+        print("Error: Image file does not exist.")
+        return None
+    try:
+        image = cv2.imread(str(image_path))
+        if image is None:
+            print("Error: Could not read the image file.")
+            return None
+        encoding = face_recognition.face_encodings(image)[0]
+        return encoding
+    except IndexError:
+        print("No face detected in the image.")
+        return None
+
+def get_image_encoding_from_csv(image_name: str, csv_filename: Path) -> list | None:
+    """
+    Get image encoding from a CSV file for a specific image.
+
+    Args:
+        image_name (str): Name of the image.
+        csv_filename (Path): Path to the CSV file containing image encodings.
+
+    Returns:
+        np.ndarray | None: The encoding of the specified image if found, None otherwise.
+    """
+    # Read the CSV file
+    with open(csv_filename, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        for row in reader:
+            if row[0] == image_name:
+                # Convert the string representation of encoding to a list of floats
+                encoding = np.array([float(value) for value in row[1].replace('[','').replace(']','').split()])
+                return encoding
+    return None
+
+
+def extract_encodings(image_folder:Path) -> dict:
+    """
+    Extract face encodings from images in a folder.
+
+    Args:
+        image_folder (str): Path to the folder containing images.
+
+    Returns:
+        dict: A dictionary containing image names as keys and their encodings as values.
+    """
+    encodings_dict = {}
+    for image_name in os.listdir(image_folder):
+        image_path = os.path.join(image_folder, image_name)
+        face_encodings = get_image_encoding(Path(image_path))
+        if face_encodings is not None:
+            if len(face_encodings) > 0:
+                encodings_dict[image_name] = face_encodings
+            else:
+                print(f"No face detected in {image_name}")
+    return encodings_dict
+
+def write_encodings_to_csv(encodings_dict:dict, csv_filename:Path)->None:
+    """
+    Write encodings dictionary to a CSV file.
+
+    Args:
+        encodings_dict (dict): Dictionary containing image names as keys and their encodings as values.
+        csv_filename (str): Name of the CSV file to be created.
+    """
+    with open(csv_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Image Name', 'encodings'])
+        for image_name, embedding in encodings_dict.items():
+            writer.writerow([image_name, embedding])
+
+def add_encoding_to_csv(image_name: str, encoding: np.ndarray, csv_filename: Path) -> None:
+    """
+    Add a new encoding to an existing CSV file.
+
+    Args:
+        image_name (str): Name of the image.
+        encoding (np.ndarray): The encoding to be added.
+        csv_filename (Path): Path to the CSV file containing image encodings.
+
+    Returns:
+        None
+    """
+    # Convert the encoding array to a string representation
+
+    # Check if the CSV file exists
+    if not csv_filename.exists():
+        print("Error: CSV file does not exist.")
+        return
+
+    # Append the new encoding to the CSV file
+    with open(csv_filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([image_name, encoding])
+    print(f"Encoding added to {csv_filename}")
+
+
+if __name__=="__main__":
+    # Usage
+    image_folder = "images"
+    csv_filename = "encodings.csv"
+
+    # Extract encodings from images in the folder
+    # encodings_dict = extract_encodings(image_folder)
+
+    # Write encodings to a CSV file
+    # write_encodings_to_csv(encodings_dict, csv_filename)
+
+    # print(f"encodings saved to {csv_filename}")
+
+    # Read encodings from the CSV file
+    encodings=extract_encodings(Path("images"))
+    print( encodings )
